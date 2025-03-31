d73ef212b2248b29e2da741d11833f01aa370fc1
kuahn.z <kuahn.z@kakaoent.com>
2025-03-31 23:08:44 +0900

    스토리 완성하기

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 package FromObjectsToFunctions
 
 import org.http4k.core.*
+import org.http4k.routing.RouterDescription
 import org.http4k.routing.bind
 import org.http4k.routing.path
 import org.http4k.routing.routes
@@ -8,24 +9,63 @@ import org.http4k.server.Jetty
 import org.http4k.server.asServer
 
 fun main() {
-    val app:HttpHandler = routes(
+    val items = listOf("write chapter", "insert code", "draw diagrams")
+    val toDoList = ToDoList(ListName("book"), items.map(::ToDoItem))
+    val lists = mapOf(User("uberto") to listOf(toDoList))
+    val app:HttpHandler = Zettai(lists)
+    app.asServer(Jetty(8080)).start()
+    println("Server started at http://localhost:8080/todo/uberto/book")
+}
+
+data class Zettai(val lists:Map<User, List<ToDoList>>): HttpHandler {
+    val routes = routes(
         "/todo/{user}/{list}" bind Method.GET to ::showlist
     )
+    override fun invoke(req: Request): Response = routes(req)
 
-    app.asServer(Jetty(8080)).start()
+    private fun showlist(request: Request): Response =
+        request.let(::extractListData)
+            .let(::fetchListContent)
+            .let(::renderHtml)
+            .let(::createResponse)
+
+    fun extractListData(request: Request): Pair<User, ListName> {
+        val user = request.path("user").orEmpty()
+        val list = request.path("list").orEmpty()
+        return User(user) to ListName(list)
+    }
+
+    fun fetchListContent(listId:Pair<User, ListName>): ToDoList =
+        lists[listId.first]?.firstOrNull { it.listName == listId.second } ?: error("List unknown")
+
+    fun renderHtml(list:ToDoList): HtmlPage =
+        HtmlPage("""
+            <html>
+                <body>
+                    <h1>Zettai</h1>
+                    <h2>${list.listName.name}</h2>
+                    <table>
+                        <tbody>
+                            ${renderItems(list.items)}
+                        </tbody>
+                    </table>
+                </body>
+            </html>
+        """.trimIndent())
+
+    fun renderItems(items: List<ToDoItem>) =
+        items.map {
+            """<tr><td>${it.description}</td></tr>""".trimIndent()
+        }.joinToString("")
+
+    fun createResponse(html:HtmlPage): Response =
+        Response(Status.OK).body(html.raw)
 }
 
-fun showlist(req: Request): Response {
-    val user:String? = req.path("user")
-    val list:String? = req.path("list")
-    val htmlPage = """
-        <html>
-            <body>
-                <h1>Zettai</h1>
-                <p>Here is the list <b>$list</b> of user <b>$user</b></p>
-            </body>
-        </html>
-    """.trimIndent()
+data class ToDoList(val listName:ListName, val items: List<ToDoItem>)
+data class ListName(val name:String)
+data class User(val name:String)
+data class ToDoItem(val description: String)
+enum class ToDoStatus { Todo, Inprogress, Done, Blocked }
+data class HtmlPage(val raw:String)
 
-    return Response(Status.OK).body(htmlPage)
-}
---
+++ src/test/kotlin/SeeATodoListAT.kt
@@ -0,0 +1,64 @@
+import FromObjectsToFunctions.*
+import org.http4k.client.JettyClient
+import org.http4k.core.Method
+import org.http4k.core.Request
+import org.http4k.core.Status
+import org.http4k.server.Jetty
+import org.http4k.server.asServer
+import org.junit.jupiter.api.Test
+import strikt.api.expectThat
+import strikt.assertions.isEqualTo
+import kotlin.test.DefaultAsserter.fail
+
+class SeeATodoListAT {
+    @Test
+    fun `List owners can see their lists`() {
+        val user = "frank"
+        val listName = "shopping"
+        val foodToBuy = listOf(ToDoItem("carrots"), ToDoItem("apples"), ToDoItem("milk"))
+
+        startTheApplication(user, listName, foodToBuy)
+
+        val list = getToDoList(user, listName)
+
+        expectThat(list.listName.name).isEqualTo(listName)
+        expectThat(list.items).isEqualTo(foodToBuy)
+    }
+
+    fun getToDoList(user:String, listName:String): ToDoList {
+        val client = JettyClient()
+        val request = Request(Method.GET, "http://localhost:8081/todo/$user/$listName")
+        val response = client(request)
+
+        return if (response.status == Status.OK) {
+            parseResponse(response.toMessage())
+        } else {
+            fail(response.toMessage())
+        }
+    }
+
+    private fun parseResponse(html: String): ToDoList {
+        val nameRegex = """<h2>.*<""".toRegex()
+        val listName = ListName(extractListName(nameRegex, html))
+        val itemsRegex = "<td>.*?<".toRegex()
+        val items = itemsRegex.findAll(html)
+            .map { ToDoItem(extractItemDesc(it)) }.toList()
+        return ToDoList(listName, items)
+    }
+
+    private fun extractListName(nameRegex:Regex, html:String):String =
+        nameRegex.find(html)?.value
+            ?.substringAfter("<h2>")
+            ?.dropLast(1)
+            .orEmpty()
+
+    private fun extractItemDesc(matchResult:MatchResult):String =
+        matchResult.value.substringAfter("<td>").dropLast(1)
+
+    private fun startTheApplication(user:String, listName: String, items: List<ToDoItem>) {
+        val toDoList = ToDoList(ListName(listName), items)
+        val lists = mapOf(User(user) to listOf(toDoList))
+        val server = Zettai(lists).asServer(Jetty(8081))
+        server.start()
+    }
+}
